c4a1578fab6b14113b8425d4b72032abdf2c4d61
hjhjhj0917 <abc22842199@gmail.com>
2025-03-29 20:51:22 +0900

    문제풀이

---
+++ src/BaekJun1546.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BaekJun1546 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int max = 0;
+        double result = 0;
+        int su = sc.nextInt();
+        int[] avr = new int[su];
+        sc.nextLine();
+
+        String line = sc.nextLine();
+        String[] str = line.split(" ");
+
+        for(int i = 0 ; i < su ; i++){
+
+            avr[i] = Integer.parseInt(str[i]);
+
+            if(avr[i] >= max){
+                max = avr[i];
+            }
+        }
+        for(int i = 0 ; i < su ; i++){
+
+            result = result + (((double) avr[i] / max) * 100);
+        }
+        System.out.println(result / su);
+
+    }
+}
