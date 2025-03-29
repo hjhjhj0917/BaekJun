de17ef29f8343620c89df2b0ed4d1e1c03936176
hjhjhj0917 <abc22842199@gmail.com>
2025-03-29 20:18:10 +0900

    문제풀이

---
+++ src/BaekJun27866.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BaekJun27866 {
+
+    // 단어 s와 정수 i가 주어졌을 때, s의 i번째 글자를 출력하는 프로그램을 작성하시오.
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
+        int su = sc.nextInt();
+
+        String[] line = str.split("");
+
+        System.out.println(line[su-1]);
+
+    }
+}
