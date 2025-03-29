14dceae44ffbc74eefdeca013922001de4fb8c47
hjhjhj0917 <abc22842199@gmail.com>
2025-03-29 20:12:35 +0900

    문제풀이

---
+++ src/BaekJun5597.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class BaekJun5597 {
+
+    // 교수님이 내준 특별과제를 28명이 제출했는데, 그 중에서 제출 안 한 학생 2명의 출석번호를 구하는 프로그램을 작성하시오.
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        HashSet<Integer> num = new HashSet<>();
+
+        for(int i = 0; i < 28 ; i++){
+
+            int su = sc.nextInt();
+            num.add(su);
+        }
+        for(int i = 1 ; i <= 30 ; i++){
+
+            if(num.contains(i)){
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
