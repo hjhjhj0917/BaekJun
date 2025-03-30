7904d737a80e91ea1e741cb077bf7733f2979117
hjhjhj0917 <abc22842199@gmail.com>
2025-03-30 21:36:34 +0900

    문제풀이

---
+++ src/Diamond.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Diamond {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for (int i = 0; i < 2 * num - 1; i++) {
+            if (i < num) {
+                for (int j = num - 1; j > i; j--) {
+                    System.out.print(" ");
+                }
+                for (int j = 0; j < 2 * i + 1; j++) {
+                    System.out.print("*");
+                }
+            } else {
+                for (int j = num - 1; j < i; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = i; j < (num-1) * 3 - (i % num); j++) {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
