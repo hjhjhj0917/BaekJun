3aa183d88f15a2be0d665d2f5ef17f19427942f3
hjhjhj0917 <abc22842199@gmail.com>
2025-04-05 22:42:36 +0900

    문제풀이

--- src/BaekJun1463.java
+++ src/BaekJun1463.java
@@ -11,12 +11,15 @@ public class BaekJun1463 {
 
         while (num > 1) {
 
-            if (num % 3 != 0) {
+            if (num % 3 == 1) {
                 num -= 1;
             } else {
-                num /= 3;
-                if (num % 2 == 0) {
+                if (num % 3 == 0) {
+                    num /= 3;
+                } else if (num % 2 == 0) {
                     num /= 2;
+                } else {
+                    num -= 1;
                 }
             }
             count++;
