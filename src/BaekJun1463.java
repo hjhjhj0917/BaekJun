879082ec55dceacc1a826ac67dd4563985e5005d
hjhjhj0917 <abc22842199@gmail.com>
2025-04-06 20:41:42 +0900

    문제풀이

--- src/BaekJun1463.java
+++ src/BaekJun1463.java
@@ -9,12 +9,19 @@ public class BaekJun1463 {
         Scanner sc = new Scanner(System.in);
 
         int num = sc.nextInt();
+        int a =  num;
         int count = 0;
+        int cnt = 0;
 
         while (num > 1) {
 
-            if (num % 3 == 1) {
-                num -= 1;
+            if (num % 3 != 0) {
+                if (num % 2 != 0) {
+                    num -= 1;
+                } else {
+                    num = num / 2;
+                }
+
             } else {
                 if (num % 3 == 0) {
                     num /= 3;
@@ -26,8 +33,22 @@ public class BaekJun1463 {
             }
             count++;
         }
+        while (a > 1) {
 
-        System.out.println(count);
+            if (a % 2 == 1) {
+                a -= 1;
+            } else {
+                a /= 2;
+            }
+            cnt++;
+        }
+        if (cnt < count) {
+            System.out.println(cnt);
+        } else if (count < cnt){
+            System.out.println(count);
+        } else {
+            System.out.println(count);
+        }
         sc.close();
     }
 }
