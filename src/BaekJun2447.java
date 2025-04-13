ebd4ee43b50a5564d57440c6cd44eb43123d358d
hjhjhj0917 <abc22842199@gmail.com>
2025-04-13 20:21:36 +0900

    문제풀이

---
+++ src/BaekJun2447.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class BaekJun2447 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for (int i = 0 ; i < num; i++) {
+            if (i < num) {
+                for (int j = 0; j < num; j++) {
+                    if (i % 2 == 1) {
+                        if (j % 3 == 1) {
+                            System.out.print(" ");
+                        } else {
+                            System.out.print("*");
+                        }
+                    } else {
+                        System.out.print("*");
+                    }
+                }
+                System.out.println();
+            }
+        }
+    }
+}
