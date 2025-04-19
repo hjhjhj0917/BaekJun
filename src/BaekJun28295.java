e23915acbcf10bbd80bd0da348245442aae5adbe
hjhjhj0917 <abc22842199@gmail.com>
2025-04-19 20:16:43 +0900

    문제풀이

---
+++ src/BaekJun28295.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class BaekJun28295 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String[] direct = {"N", "E", "S", "W"};
+        String res = "N";
+        int loc = 0;
+
+        for (int i = 0; i < 10; i++) {
+            int num = sc.nextInt();
+
+            if (num == 1) {
+                res = direct[(loc + num)%4];
+            } else if (num == 2) {
+                res = direct[(loc + num)%4];
+            } else if (num == 3) {
+                res = direct[(loc + num)%4];
+            }
+            loc += num;
+        }
+        System.out.println(res);
+        sc.close();
+    }
+}
