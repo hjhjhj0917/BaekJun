fa84ee961b0544019f4c6d2565c07ab45690affb
hjhjhj0917 <abc22842199@gmail.com>
2025-05-10 18:59:51 +0900

    문제풀이

---
+++ src/Programers/Pro120836.java
@@ -0,0 +1,24 @@
+package Programers;
+
+import java.util.Scanner;
+
+public class Pro120836 {
+
+    static int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                answer++;
+            }
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        System.out.println(solution(n));
+    }
+}
