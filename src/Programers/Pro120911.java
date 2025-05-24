bdc242b7a1d06010e9a99dfa7f46f09de2ee17b8
hjhjhj0917 <abc22842199@gmail.com>
2025-05-24 21:11:48 +0900

    문제풀이

---
+++ src/Programers/Pro120911.java
@@ -0,0 +1,26 @@
+package Programers;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Pro120911 {
+
+    static String solution(String my_string) {
+        String answer = "";
+
+        char[] my_char = my_string.toLowerCase().toCharArray();
+
+        Arrays.sort(my_char);
+
+        answer = new String(my_char);
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String my_string = sc.nextLine();
+        System.out.println(solution(my_string));
+    }
+}
