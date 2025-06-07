a2fd1ceedf3dca650046766b7930be0fd773cbff
hjhjhj0917 <abc22842199@gmail.com>
2025-06-07 19:36:36 +0900

    문제풀이

---
+++ src/Programers/Pro181947.java
@@ -0,0 +1,14 @@
+package Programers;
+
+import java.util.Scanner;
+
+public class Pro181947 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a + " + " + b + " = " + (a + b));
+    }
+}
