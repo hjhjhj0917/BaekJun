5c93044da87ed400b8c859fb67fe0f388bde7ba3
hjhjhj0917 <abc22842199@gmail.com>
2025-06-08 17:10:50 +0900

    문제풀이

---
+++ src/Programers/Pro181945.java
@@ -0,0 +1,15 @@
+package Programers;
+
+import java.util.Scanner;
+
+public class Pro181945 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+
+        for (char c : a.toCharArray()) {
+            System.out.println(c);
+        }
+    }
+}
