6a4dafd113f31c6ae4d3b585d9ee8e1797ac21cf
hjhjhj0917 <abc22842199@gmail.com>
2025-06-21 17:22:54 +0900

    문제풀이

---
+++ src/Programers/Pro181845.java
@@ -0,0 +1,21 @@
+package Programers;
+
+import java.util.Scanner;
+
+public class Pro181845 {
+
+    static String solution(int n) {
+        String answer = "";
+
+        answer = String.valueOf(n);
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        System.out.println(solution(n));
+    }
+}
