fa7ba65d111de279eda2102c1570d8ab310b368e
hjhjhj0917 <abc22842199@gmail.com>
2026-01-16 23:10:09 +0900

    문제풀이

--- src/BaekJun/BaekJun2914.java
+++ src/BaekJun/BaekJun2914.java
@@ -6,6 +6,10 @@ public class BaekJun2914 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int apple = sc.nextInt();
 
+        for (int i = 1; i <= apple; i++) {
+            System.out.println("Apple");
+        }
     }
 }
