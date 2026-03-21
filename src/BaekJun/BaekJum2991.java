0201689caaf113ac3d302993556eaf64fac2c96e
hjhjhj0917 <abc22842199@gmail.com>
2026-03-21 19:12:37 +0900

    문제풀이

---
+++ src/BaekJun/BaekJum2991.java
@@ -0,0 +1,27 @@
+package BaekJun;
+
+import java.util.Scanner;
+
+public class BaekJum2991 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+
+        int min1 = sc.nextInt();
+        int min2 = sc.nextInt();
+        int min3 = sc.nextInt();
+        int min4 = sc.nextInt();
+
+        int p = sc.nextInt();
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+
+
+        while (true) {
+
+
+
+        }
+    }
+}
