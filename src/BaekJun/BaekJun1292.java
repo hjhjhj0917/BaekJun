e2164cd4260b2332a875d78490a2f6b3823b2b5f
hjhjhj0917 <abc22842199@gmail.com>
2026-03-29 19:19:30 +0900

    문제풀이

---
+++ src/BaekJun/BaekJun1292.java
@@ -0,0 +1,27 @@
+package BaekJun;
+
+import java.util.Scanner;
+
+public class BaekJun1292 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int startNum = sc.nextInt();
+        int endNum = sc.nextInt();
+        int cnt = 0;
+        int result = 0;
+
+        for (int i = 1; i <= endNum; i++) {
+            for (int j = 0; j < i; j++) {
+
+                cnt += 1;
+
+                if (cnt >= startNum && cnt <= endNum) {
+                    result += i;
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}
