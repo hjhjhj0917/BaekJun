9fcf6cf1dae7d597a10f3e841b588cf8b4359b07
hjhjhj0917 <abc22842199@gmail.com>
2026-04-26 17:07:30 +0900

    문제풀이

---
+++ src/Programers/Pro120810.java
@@ -0,0 +1,21 @@
+package Programers;
+
+import java.util.Scanner;
+
+public class Pro120810 {
+    public static int solution(int num1, int num2) {
+
+        int answer = num1 % num2;
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+
+        System.out.println(solution(num1, num2));
+    }
+}
