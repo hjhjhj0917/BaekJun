94c0044908fd42b5a06a95254650c2b26615e64c
hjhjhj0917 <abc22842199@gmail.com>
2026-04-27 18:03:03 +0900

    문제풀이

---
+++ src/Programers/Pro120583.java
@@ -0,0 +1,23 @@
+package Programers;
+
+import java.util.Scanner;
+
+public class Pro120583 {
+    public static int solution(int[] array, int n) {
+        int answer = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == n) answer++;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] array = {1, 1, 2, 3, 4, 5};
+        int n = sc.nextInt();
+
+        System.out.println(solution(array, n));
+    }
+}
